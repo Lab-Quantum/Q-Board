3681d836359deefb004db4b6688ea02c8f5b9950
Cristian raffi Cunha <contato@cristiancunha.me>
2020-11-26 08:22:02 -0300

    Create description Page

--- src/components/GridPage/styles.js
+++ src/components/GridPage/styles.js
@@ -4,6 +4,10 @@ const useStyles = makeStyles(() => ({
   gridContainer: {
     width: 'calc(100vw - 80px)',
     minHeight: '100vh',
+
+    display: 'flex',
+
+    padding: '30px 0px 10px 0px',
   },
 }));
 export default useStyles;
